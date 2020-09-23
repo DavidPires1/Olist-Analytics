383db990462c0831175d9ababf6e2c6900ec8713
DavidPires1 <d.cotrim17@gmail.com>
2020-09-23 14:13:54 -0300

    Modificação lifetime sql

--- src/sql/lifetime.sql
+++ src/sql/lifetime.sql
@@ -1,36 +1,45 @@
-SELECT *,
-        PERCENT_RANK() OVER (PARTITION BY product_category_name ORDER BY days_no_sale) AS PCT_ACUM
+SELECT *
 
 FROM(
 
     SELECT *,
-            julianday(DATE(order_approved_at)) - julianday(DATE(last_sale)) AS days_no_sale
+            PERCENT_RANK() OVER (PARTITION BY product_category_name ORDER BY days_no_sale) AS PCT_ACUM,
+            ROW_NUMBER() OVER (PARTITION BY seller_id, product_category_name ORDER BY RANDOM()) AS RANDOM
 
     FROM(
 
-        SELECT T2.seller_id,
-                T3.product_category_name,
-                T1.order_approved_at,
-                LAG(T1.order_approved_at) OVER ( PARTITION BY T2.seller_id, 
-                T3.product_category_name ORDER BY T1.order_approved_at) AS last_sale
-            
+        SELECT *,
+                julianday(DATE(order_approved_at)) - julianday(DATE(last_sale)) AS days_no_sale
 
-        FROM
-                        
-        tb_orders AS T1
-        LEFT JOIN
-        tb_order_items AS T2 ON T1.order_id = T2.order_id
-        LEFT JOIN
-        tb_products AS T3 ON T2.product_id = T3.product_id 
+        FROM(
 
-        WHERE T2.seller_id IS NOT NULL
-        AND T3.product_category_name IS NOT NULL
+            SELECT T2.seller_id,
+                    T3.product_category_name,
+                    T1.order_approved_at,
+                    LAG(T1.order_approved_at) OVER ( PARTITION BY T2.seller_id, 
+                    T3.product_category_name ORDER BY T1.order_approved_at) AS last_sale
+                
 
-        ORDER BY T2.seller_id, T1.order_approved_at
+            FROM
+                            
+            tb_orders AS T1
+            LEFT JOIN
+            tb_order_items AS T2 ON T1.order_id = T2.order_id
+            LEFT JOIN
+            tb_products AS T3 ON T2.product_id = T3.product_id 
 
-    ) AS T1
+            WHERE T2.seller_id IS NOT NULL
+            AND T3.product_category_name IS NOT NULL
 
-    WHERE T1.last_sale IS NOt NULL
-    AND days_no_sale >= 1
+            ORDER BY T2.seller_id, T1.order_approved_at
+
+        ) AS T1
+
+        WHERE T1.last_sale IS NOt NULL
+        AND days_no_sale >= 1
+
+    )AS T1
 
 )AS T1
+
+WHERE RANDOM = 1
